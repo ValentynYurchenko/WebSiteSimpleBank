b1db913c2af0aeb6cc8f8f586169655b99886d9a
ValentynYurchenko <valentin.urchenko@gmail.com>
2023-05-03 19:51:05 +0300

    select, creation, insertion and removal of DOM elements

--- script.js
+++ script.js
@@ -36,3 +36,45 @@ document.addEventListener('keydown', function (e) {
     closeModalWindow();
   }
 });
+
+////////////////////////////////////////////////////////////
+// Выбор элементов
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+console.log(document.querySelector('.header'));
+const sections = document.querySelectorAll('.section');
+console.log(sections);
+
+console.log(document.getElementById('section--1'));
+const buttons = document.getElementsByTagName('button');
+console.log(buttons);
+
+console.log(document.getElementsByClassName('btn'));
+
+////////////////////////////////////////////////////////////
+// Создание и вставка элементов
+// .insertAdjacentHTML()
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent =
+//   'Мы используем на этом сайте cookie для улучшения функциональности.';
+message.innerHTML =
+  'Мы используем на этом сайте cookie для улучшения функциональности. <button class="btn btn--close-cookie">Ok!</button>';
+
+const header = document.querySelector('.header');
+header.prepend(message); // добавляет элемент message в качестве первого дочернего элемента header
+header.append(message); // добавляет элемент message в качестве последнего дочернего элемента header (при этом message не может находиться в двух местах одновременно. Он просто переместился с начала header в конец).
+// header.append(message.cloneNode(true)); // таким образом элемент message клонируеться и будет отображаться и в начале header и в конце.
+// header.before(message);
+// header.after(message);
+
+// Удаление элементов
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+    // message.parentElement.removeChild(message); // старый способ удаления елементов
+  });
